afddff81dc9e3bf9ac2ebd64f3acf1ab9c66c5a4
Abdallah-Elsaid19 <abdoelsaid863@gmail.com>
2026-02-15 01:36:54 +0200

    updata image

--- src/app/doctors/page.js
+++ src/app/doctors/page.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Doctors() {
   return (
     <div className='bg-gray-50 dark:bg-black'>
-     <div className='h-30 md:h-50 w-full bg-contain bg-no-repeat'
+     <div className='h-30 md:h-50 w-full bg-contain md:bg-cover bg-no-repeat'
       style={{backgroundImage:"url('/ourDoctors.png')"}}
      > 
        <h2 className='row pt-5 md: lg:pt-20 text-lg font-bold'> Our Doctors </h2>
